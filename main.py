f1dc43facf914daee13032e67657b6785ec8448c
Hugh Jaiden <hugh.j@ku.th>
2026-03-14 20:33:52 +0700

    Reverted viewport to Rect and added resizing with borders

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 class MainGame:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((200, 200))
+        self.screen = pygame.display.set_mode((200, 200), pygame.RESIZABLE)
         self.clock = pygame.Clock()
         self.running = True
         self.delta_time = 0
-        self.renderManager = RenderManager((300,300))
         self.map = Map()
-        self.map.generate_map()
+        self.map.generate_map((300, 300))
+        self.renderManager = RenderManager(self.map)
         self.mainloop()
 
     def mainloop(self):
@@ -18,7 +18,6 @@ class MainGame:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            self.screen.fill("#000000")
             self.renderManager.render(self.map, self.screen)
             pygame.display.flip()
 
@@ -26,26 +25,33 @@ class MainGame:
 
 class Map:
     def __init__(self):
+        self.map_size = None
         self.planets: list[Planet] = []
 
-    def generate_map(self):
+    def generate_map(self, map_size):
+        self.map_size = map_size
         self.planets.append(Planet((50, 60)))
 
-    def render(self, surface: pygame.Surface, viewport: pygame.Surface):
+    def render(self, surface: pygame.Surface, viewport: pygame.Rect):
         # viewport for culling or something idk im doing premature optimisation im so dead
-        for visible_planet in viewport.get_rect().collideobjectsall(self.planets, key=lambda o: o.rect):
+        for visible_planet in viewport.collideobjectsall(self.planets, key=lambda o: o.rect):
             visible_planet: Planet
             visible_planet.render(surface)
         return surface
 
 class RenderManager:
-    def __init__(self, map_size: tuple):
-        self.surface = pygame.Surface(map_size)
-        self.viewport = self.surface.subsurface(pygame.Rect(0, 0, 100, 100))
+    def __init__(self, map: Map):
+        self.surface = pygame.Surface(map.map_size)
+        self.viewport = pygame.Rect(0, 0, 100, 100)
 
     def render(self, map: Map, screen: pygame.Surface):
+        screen.fill("#000000")
+        viewport_surface = self.surface.subsurface(self.viewport)
+        viewport_surface.fill("#777777")
         self.surface = map.render(self.surface, self.viewport)
-        pygame.transform.scale(self.viewport, screen.size, dest_surface=screen) # woah blitting by scaling this is so cool
+        scale_ratio = min(screen.size[0]/self.viewport.width, screen.size[1]/self.viewport.height)
+        scaled_viewport = pygame.transform.scale_by(viewport_surface, scale_ratio)
+        screen.blit(scaled_viewport, ((screen.size[0]-scaled_viewport.size[0])/2, (screen.size[1]-scaled_viewport.size[1])/2))
 
 class Planet:
     def __init__(self, position: tuple, size=10):
